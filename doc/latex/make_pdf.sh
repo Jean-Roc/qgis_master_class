556abd9cb08b862033378cf9feafa6c40cf428ce
Jean-Roc Morreale <jr.morreale@enoreth.net>
2013-05-02 15:26:07 +0200

    nettoyage

--- doc/latex/make_pdf.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-rm -rf *.aux *.bbl *.blg *.idx *.ilg *.ind *.lof *.log *.lot *.toa *.toc *.synctex*.gz *.html *.4ct *.4tc *.css *.dvi *.idv *.tmp *.xref *.lg *.out *.png
-pdflatex sigll.tex
-pdflatex sigll.tex
-rm -rf *.aux *.bbl *.blg *.idx *.ilg *.ind *.lof *.log *.lot *.toa *.toc *.synctex*.gz *.html *.4ct *.4tc *.css *.dvi *.idv *.tmp *.xref *.lg *.out *.png
